a5bbb294ae67b74c4fb22c5a964d39bebd30b65d
capogreco <thomas.capogreco@gmail.com>
2023-03-24 11:37:00 +1100

    added pink square

--- public/sketch.js
+++ public/sketch.js
@@ -3,8 +3,13 @@ document.body.style.overflow = `hidden`
 
 function setup () {
    createCanvas (innerWidth, innerHeight)
+   noLoop ()
+   noStroke ()
+   fill (`deeppink`)
+   rectMode (CENTER)
 }
 
 function  draw () {
    background (`turquoise`)
+   square (width / 2, height / 2, 100)
 }
